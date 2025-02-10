1e60322f75d3de5b85120511e605eb56009bf86f
mateusgdmelo <mateusgomessdemelo@gmail.com>
2025-02-10 06:53:33 -0300

    aula004 - comentários em Linguagem C

---
+++ aulas/aula004/aula004.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <locale.h>
+
+void main() {
+    setlocale(LC_ALL, "Portuguese");
+    // Comentários em linguagem C
+
+    // Usamos esse tipo de comentário para comentários de uma linha única
+
+    /* Usamos esse tipo de comentário
+    para comentários de
+    múltiplas linhas.*/
+
+
+}
