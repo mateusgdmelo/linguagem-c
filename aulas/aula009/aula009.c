8852822c352bbfdfefe3bb3c0585787d07a75600
Mateus Melo <mateusgomessdemelo@gmail.com>
2025-02-16 16:22:28 -0300

    Aula009 - Números Aleatórios

--- aulas/aula009/aula009.c
+++ aulas/aula009/aula009.c
@@ -8,7 +8,7 @@ void main() {
   setlocale(LC_ALL, "Portuguese");
   srand(time(NULL));
 
-  int n = rand() % 10 //gera números de 0 a 9
+  int n = rand() % 10; //gera números de 0 a 9
 
   //obs.: para gerar números de 1 a 10, use: % 10 + 1; de 1 a 50, use: % 50 + 1, e assim por diante.
 
